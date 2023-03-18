32011cefa95568721fe5870883f363d0cae95805
Pasi-dev.xyz <pascalpetzold06@gmail.com>
2023-03-18 21:25:12 +0100

    Deleted all because of Privacy code is up soon

--- src/database/models/warnings.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const Schema = new mongoose.Schema({
-    Guild: String,
-    User: String,
-    Warns: Number
-});
-
-module.exports = mongoose.model("warnings", Schema);
